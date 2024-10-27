a56fffedcadb9ddbe0f59426e021f25f33490913
muheOS <muhe0003@gmail.com>
2024-10-27 14:37:17 +0800

    提交测试

---
+++ lab2/obj/kern/mm/slub_pmm_not.d
@@ -0,0 +1,4 @@
+obj/kern/mm/slub_pmm_not.o obj/kern/mm/slub_pmm_not.d: \
+ kern/mm/slub_pmm_not.c kern/mm/pmm.h kern/debug/assert.h libs/defs.h \
+ libs/atomic.h kern/mm/memlayout.h libs/list.h kern/mm/mmu.h libs/riscv.h \
+ libs/string.h kern/mm/slub_pmm.h libs/stdio.h libs/stdarg.h
