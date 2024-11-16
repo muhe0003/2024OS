c36757d198e1c70fdc6aafcf3ad6ee1819f8a5fa
Zhaoyuanming <2938731397@qq.com>
2024-11-16 19:59:13 +0800

    swap clock implement

--- lab3/kern/mm/swap_clock.c
+++ lab3/kern/mm/swap_clock.c
@@ -17,7 +17,7 @@
  *
  * Details of FIFO PRA
  * (1) Prepare: In order to implement FIFO PRA, we should manage all swappable pages, so we can
- *              link these pages into pra_list_head according the time order. At first you should
+ *              link these pages into pra_list_head_c according the time order. At first you should
  *              be familiar to the struct list in list.h. struct list is a simple doubly linked list
  *              implementation. You should know howto USE: list_init, list_add(list_add_after),
  *              list_add_before, list_del, list_next, list_prev. Another tricky method is to transform
@@ -25,57 +25,84 @@
  *              le2page (in memlayout.h), (in future labs: le2vma (in vmm.h), le2proc (in proc.h),etc.
  */
 
-list_entry_t pra_list_head, *curr_ptr;
+list_entry_t pra_list_head_c, *curr_ptr;
 /*
- * (2) _fifo_init_mm: init pra_list_head and let  mm->sm_priv point to the addr of pra_list_head.
+ * (2) _fifo_init_mm: init pra_list_head_c and let  mm->sm_priv point to the addr of pra_list_head_c.
  *              Now, From the memory control struct mm_struct, we can access FIFO PRA
  */
 static int
 _clock_init_mm(struct mm_struct *mm)
 {     
-     /*LAB3 EXERCISE 4: YOUR CODE*/ 
-     // 初始化pra_list_head为空链表
-     // 初始化当前指针curr_ptr指向pra_list_head，表示当前页面替换位置为链表头
-     // 将mm的私有成员指针指向pra_list_head，用于后续的页面替换算法操作
+    /*LAB3 EXERCISE 4: YOUR CODE*/ 
+     // 初始化pra_list_head_c为空链表
+     // 初始化当前指针curr_ptr指向pra_list_head_c，表示当前页面替换位置为链表头
+     // 将mm的私有成员指针指向pra_list_head_c，用于后续的页面替换算法操作
+
+    list_init(&pra_list_head_c);
+    mm->sm_priv = &pra_list_head_c;
+    curr_ptr = &pra_list_head_c;
+    
+
      //cprintf(" mm->sm_priv %x in fifo_init_mm\n",mm->sm_priv);
-     return 0;
+    return 0;
 }
 /*
- * (3)_fifo_map_swappable: According FIFO PRA, we should link the most recent arrival page at the back of pra_list_head qeueue
+ * (3)_fifo_map_swappable: According FIFO PRA, we should link the most recent arrival page at the back of pra_list_head_c qeueue
  */
 static int
 _clock_map_swappable(struct mm_struct *mm, uintptr_t addr, struct Page *page, int swap_in)
 {
     list_entry_t *entry=&(page->pra_page_link);
- 
+
     assert(entry != NULL && curr_ptr != NULL);
     //record the page access situlation
     /*LAB3 EXERCISE 4: YOUR CODE*/ 
-    // link the most recent arrival page at the back of the pra_list_head qeueue.
-    // 将页面page插入到页面链表pra_list_head的末尾
+    // link the most recent arrival page at the back of the pra_list_head_c qeueue.
+    // 将页面page插入到页面链表pra_list_head_c的末尾
     // 将页面的visited标志置为1，表示该页面已被访问
+    // list_add_before(&pra_list_head_c,entry);
+    list_add_before((list_entry_t*) mm->sm_priv,entry);
+    page->visited = 1;
     return 0;
 }
 /*
- *  (4)_fifo_swap_out_victim: According FIFO PRA, we should unlink the  earliest arrival page in front of pra_list_head qeueue,
+ *  (4)_fifo_swap_out_victim: According FIFO PRA, we should unlink the  earliest arrival page in front of pra_list_head_c qeueue,
  *                            then set the addr of addr of this page to ptr_page.
  */
 static int
 _clock_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick)
 {
      list_entry_t *head=(list_entry_t*) mm->sm_priv;
-         assert(head != NULL);
-     assert(in_tick==0);
+        assert(head != NULL);
+    assert(in_tick==0);
      /* Select the victim */
-     //(1)  unlink the  earliest arrival page in front of pra_list_head qeueue
+     //(1)  unlink the  earliest arrival page in front of pra_list_head_c qeueue
      //(2)  set the addr of addr of this page to ptr_page
     while (1) {
         /*LAB3 EXERCISE 4: YOUR CODE*/ 
         // 编写代码
-        // 遍历页面链表pra_list_head，查找最早未被访问的页面
+        // 遍历页面链表pra_list_head_c，查找最早未被访问的页面
         // 获取当前页面对应的Page结构指针
         // 如果当前页面未被访问，则将该页面从页面链表中删除，并将该页面指针赋值给ptr_page作为换出页面
         // 如果当前页面已被访问，则将visited标志置为0，表示该页面已被重新访问
+        curr_ptr = list_next(curr_ptr);
+        //check if list is empty
+        if(curr_ptr == head) {
+            curr_ptr = list_next(curr_ptr);
+            if(curr_ptr == head) {
+                *ptr_page = NULL;
+                break;
+            }
+        }
+        //make list entry a page
+        struct Page* page = le2page(curr_ptr, pra_page_link);
+        if(page->visited==1) {
+            page->visited = 0;
+        } else {
+            *ptr_page = page;
+            list_del(curr_ptr);
+            break;
+        }
     }
     return 0;
 }
